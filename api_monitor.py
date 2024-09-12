2e1c5296fd02b170200f6576aed64212872fd68d
Mikias Worash <mikiasworash@gmail.com>
2024-09-12 14:22:38 +0300

    added api monitor script

---
+++ api_monitor.py
@@ -0,0 +1,34 @@
+import requests
+import os
+import time
+
+
+API_URL = "https://comart-api.onrender.com/api/ratings"
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN") 
+TELEGRAM_CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+def check_api():
+    try:
+        response = requests.get(API_URL, timeout=10)
+        if response.status_code != 200:
+            send_alert(f"🔴 API DOWN: Status Code {response.status_code}")
+        else:
+            # send_alert(f"✅ API OK: Status Code {response.status_code}")
+            print(f"✅ API OK: {response.status_code}")
+    except requests.exceptions.RequestException as e:
+        send_alert(f"🔴 API ERROR: {str(e)}")
+
+def send_alert(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message
+    }
+    try:
+        requests.post(url, json=payload)
+        print(f"Alert sent: {message}")
+    except Exception as e:
+        print(f"Failed to send Telegram alert: {e}")
+
+if __name__ == "__main__":
+    check_api()
